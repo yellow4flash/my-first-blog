b5205b3bf3530c9277e85342612023f4f72acc15
yelow4flash <minatonamikaze2020@gmail.com>
2019-03-14 15:09:45 -0500

    modify settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ezaqvzw1axbap0qk%!)ug!072c=wtp6j6zku^_w9yb4f=3q$=('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','. pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','yellow4flash.pythonanywhere.com']
 
 
 # Application definition
